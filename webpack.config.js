b2a0c8bbed2c9bd6a9d235448528ed01746666e7
tenmihi <tenmihi@gmail.com>
2017-08-27 15:27:56 +0900

    init(simple-parser): first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  cache: true,
+  entry: 'parser.js',
+  output: {
+    path: path.resolve(__dirname, './'),
+    filename: "index.js",
+  },
+  resolve: {
+    modules: [ path.resolve(__dirname, './src/js') ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ]
+  },
+}
